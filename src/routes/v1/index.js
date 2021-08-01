cf3d94aceae389744c6f4b162d84bf13bbe1cecd
Ajay M <ajay@shivy.co.in>
2021-08-02 03:40:09 +0530

    Removed auth route

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,14 +1,9 @@
 const express = require('express');
-const authRoute = require('./auth.route');
 const roomRoute = require('./room.route');
 
 const router = express.Router();
 
 const defaultRoutes = [
-  {
-    path: '/auth',
-    route: authRoute,
-  },
   {
     path: '/rooms',
     route: roomRoute,
